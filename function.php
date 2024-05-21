7647e4536e1f60c3bf95315851c815af7baee496
PRACHI <152630353+riypr@users.noreply.github.com>
2024-05-22 01:38:05 +0530

    Update function.php

--- function.php
+++ function.php
@@ -1,11 +1,11 @@
 function check_login($con) {
     if (isset($_SESSION['user_id'])) {
-        $id = $_SESSION['user_id'];
-        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1";
+        $id = SESSION['user_id']
+        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1;
 
         $result = mysqli_query($con, $query);
-        if ($result && mysqli_num_rows($result) > 0) {
-            return mysqli_fetch_assoc($result);
+        if ($result mysqli_num_rows($result) > 0) {
+            return mysqli_fetch_assoc($result)
         }
     }
 
